9e305bce09db5787357220c6b266091924ea39d5
Denis Vojtenko <vojtenkoda@mts.ru>
2025-12-05 21:21:53 +0300

    Init Projects

---
+++ admin/src/test/java/ru/mipt/movies/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mipt.movies.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ content/src/test/java/ru/mipt/movies/content/ContentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mipt.movies.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
